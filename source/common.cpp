b19e1dc943d2d9b161f800467eea275e4836aa96
Gavin <gavin.blakeman@theblakemans.id.au>
2018-09-24 20:59:59 +0800

    Updating directory names

--- source/common.cpp
+++ source/common.cpp
@@ -56,7 +56,7 @@ namespace MCL
 
   unsigned int const MCL_MAJORVERSION = 2018;           ///< The Major version number (yyyy)
   unsigned int const MCL_MINORVERSION = 9;              ///< The Minor version number (mm)
-  unsigned int const MCL_BUILDNUMBER  = 0x007A;         ///< The build numnber - incremented on each build.
+  unsigned int const MCL_BUILDNUMBER  = 0x0090;         ///< The build numnber - incremented on each build.
 
   /// @brief Returns the library version and build number that was used for editing the library.
   /// @returns A string with the application name, version, build.
