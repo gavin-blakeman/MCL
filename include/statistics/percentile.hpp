38861dc5d77c52b30e12746791ee24366fbf7d3b
Gavin Blakeman <gavin.blakeman@theblakemans.id.au>
2023-11-23 17:06:19 +0800

    Added test case for percentile (vector)

--- include/statistics/percentile.hpp
+++ include/statistics/percentile.hpp
@@ -1,4 +1,40 @@
-﻿#ifndef PERCENTILE_HPP
+﻿//*********************************************************************************************************************************
+//
+// PROJECT:							Math Class Library
+// FILE:								percentile
+// SUBSYSTEM:						Statistics Functions.
+// LANGUAGE:						C++
+// TARGET OS:						None.
+// LIBRARY DEPENDANCE:	None.
+// NAMESPACE:						MCL
+// AUTHOR:							Gavin Blakeman.
+// LICENSE:             GPLv2
+//
+//                      Copyright 2022-2023 Gavin Blakeman.
+//                      This file is part of the Maths Class Library (MCL)
+//
+//                      MCL is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+//                      License as published by the Free Software Foundation, either version 2 of the License, or (at your option)
+//                      any later version.
+//
+//                      MCL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+//                      warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//                      more details.
+//
+//                      You should have received a copy of the GNU General Public License along with MCL.  If not, see
+//                      <http://www.gnu.org/licenses/>.
+//
+// OVERVIEW:            Implementation of templated and multi-threaded statistics functions.
+//
+// CLASSES INCLUDED:    None
+//
+// FUNCTIONS INCLUDED:  median(...)
+//
+// HISTORY:             2022-12-02 GGB - File Created.
+//
+//*********************************************************************************************************************************
+
+#ifndef PERCENTILE_HPP
 #define PERCENTILE_HPP
 
   // Standard C++ library
@@ -6,20 +42,18 @@
 #include <algorithm>
 #include <cmath>
 #include <valarray>
+#include <vector>
 
 #include "../config.h"
 
 namespace MCL
 {
 
-  /// @brief Returns the percentile requested from the array.
-  /// @note 1. This is an expensive function. The valarray is copied to the function. Once copied the valarray is sorte and then the
-  ///          percentile extracted.
-  /// @note 2. See also the median and mode functions. That also require to valarray to be sorted.
-  /// @param[in] va: The valarray to find the percentile.
-  /// @param[in] p: The percentile to find. [0; 100]
+  /// @brief      Returns the percentile requested from the array.
+  /// @param[in]  va: The valarray to find the percentile.
+  /// @param[in]  p: The percentile to find. [0; 100]
   /// @throws
-  /// @version 2022-12-02/GGB - Function created.
+  /// @version    2022-12-02/GGB - Function created.
 
   template<typename T, typename U>
   FP_t percentile(std::valarray<T> va, U p)
@@ -55,6 +89,50 @@ namespace MCL
         d -= std::floor(d);
         d *= (va[s2] - va[s1]);
         return va[s1] + d;
+      }
+    };
+  }
+
+  /// @brief      Returns the percentile requested from the array.
+  /// @param[in]  v: The vector of values. to find the percentile.
+  /// @param[in]  p: The percentile to find. [0; 100]
+  /// @throws
+  /// @version    2023-11-23/GGB - Function created.
+
+  template<typename T, typename U>
+  FP_t percentile(std::vector<T> v, U p)
+  {
+    if (p == 0)
+    {
+      return 0;
+    }
+    else if (v.size() == 0)
+    {
+      return 0;
+    }
+    else
+    {
+      std::size_t s1 = 0;
+      std::size_t s2 = 0;
+
+      std::sort(std::begin(v), std::end(v));
+
+      FP_t d = static_cast<FP_t>(p) * static_cast<FP_t>(v.size() + 1);
+      s1 = static_cast<std::size_t>(std::floor(d)) - 1;
+      s2 = static_cast<std::size_t>(std::ceil(d)) - 1;
+
+      s1 = std::min(s1, v.size() - 1);
+      s2 = std::min(s2, v.size() - 1);
+
+      if (s1 == s2)
+      {
+        return v[s1];
+      }
+      else
+      {
+        d -= std::floor(d);
+        d *= (v[s2] - v[s1]);
+        return v[s1] + d;
       }
     };
   }
--- test/statistics/percentile_test.cpp
+++ test/statistics/percentile_test.cpp
@@ -42,4 +42,39 @@ BOOST_AUTO_TEST_CASE(valarray, *boost::unit_test::tolerance(0.001))
   BOOST_TEST(MCL::percentile(B, 0.90) == 22.7);
 }
 
+BOOST_AUTO_TEST_CASE(vector, *boost::unit_test::tolerance(0.001))
+{
+  std::vector<int> X = { 97, 82, 52, 17, 7, 59, 10, 20, 37, 1, 57, 50, 56, 50, 99, 24, 4, 58, 98, 15, 78, 65, 63, 70, 96,
+                              7, 54, 24, 10, 80, 45, 2, 62, 4, 13, 18, 17, 79 };
+  std::vector<int> A = { 97, 82, 52, 17, 7, 59, 10, 20, 37, 1, 57, 50, 56, 50, 99, 24, 4, 58, 98, 15, 78, 65, 63, 70, 96,
+                              7, 54, 24, 10, 80, 45, 2, 62, 4, 13, 18, 17 };
+
+  std::vector<double> Y = { 46.9, 8.7, 8.2, 40.5, 95.4, 63.3, 10.3, 12.6, 30.5, 61.9, 61.2, 59.9, 65.2, 90.2, 98.0, 16.3, 4.4,
+                              29.2, 71.2, 71.2, 76.1, 81.1, 15.9, 67.5, 19.6, 70.6, 37.4, 21.4, 88.3, 16.4, 37.0, 66.8, 36.3, 65.1,
+                              42.6, 67.0, 44.3 };
+  std::vector<double> Z = { 46.9, 8.7, 8.2, 40.5, 95.4, 63.3, 10.3, 12.6, 30.5, 61.9, 61.2, 59.9, 65.2, 90.2, 98.0, 16.3, 4.4,
+                              29.2, 71.2, 71.2, 76.10, 81.1, 15.9, 67.5, 19.6, 70.6, 37.4, 21.4, 88.3, 16.4, 37.0, 66.8, 36.3, 65.1,
+                              42.6, 67.0 };
+
+  std::vector<std::uint16_t> B = { 6, 6, 7, 6, 6, 5, 4, 6, 7, 5, 3, 5, 4, 8, 6, 4, 4, 6, 4, 5, 4, 5, 26, 15, 9, 6, 57, 37, 5,
+                                     9, 6, 8 };
+
+  BOOST_TEST(MCL::percentile(X, 0.1) == 4);
+  BOOST_TEST(MCL::percentile(X, 0.2) == 10);
+  BOOST_TEST(MCL::percentile(X, 0.5) == 50);
+  BOOST_TEST(MCL::percentile(X, 0.9) == 96.1);
+
+  BOOST_TEST(MCL::percentile(A, 0.8) == 73.2);
+  BOOST_TEST(MCL::percentile(A, 0.9) == 96.2);
+
+  BOOST_TEST(MCL::percentile(Y, 0.5) == 46.9);
+  BOOST_TEST(MCL::percentile(Y, 0.75) == 69.1);
+  BOOST_TEST(MCL::percentile(Y, 0.95) == 95.7);
+
+  BOOST_TEST(MCL::percentile(Z, 0.75) == 69.851);
+  BOOST_TEST(MCL::percentile(Z, 0.85) == 78.3);
+
+  BOOST_TEST(MCL::percentile(B, 0.90) == 22.7);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
